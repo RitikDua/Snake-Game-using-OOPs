2c11028ac583566c4b0988c02415e956507d0dc9
Ritik <ritik.cse18@chitkarauniversity.edu.in>
2020-04-27 03:56:22 +0530

    initial drawing of snake

--- snake.js
+++ snake.js
@@ -10,7 +10,6 @@ class Snake{
 		return "snake";
 	}
 
-
 }
 
 class Move{
@@ -25,11 +24,37 @@ class Move{
 			console.log("snake");
 		}
 		else{
-			console.log("something"+(this.runner.type);
+			console.log("something"+(this.runner.type));
 		}
 	}
 }
 
-let x=new Snake(1,1);
-let m=new Move(x);//.move();
-m.move(1,1);
+
+class Draw{
+	drawSnakePart(snakePart){
+		ctx.fillStyle="lightgreen";
+		ctx.strokeStyle="darkgreen";
+		ctx.fillRect(snakePart.x,snakePart.y,10,10);
+		ctx.strokeRect(snakePart.x,snakePart.y,10,10);
+			
+	}
+	drawSnake(snake){
+		snake.pos.forEach(this.drawSnakePart);
+	}
+	drawSomthing(thing){
+		console.log("drawSometghing"+thing.type);
+	}
+	 draw(thing){
+		console.log((thing.type==="snake"));
+		if(thing.type==="snake")
+		{
+			this.drawSnake(thing);
+		}
+		else{
+			this.drawSomthing(thing);
+		}
+	}
+}
+let snakeTest= new Snake([  {x: 150, y: 150},  {x: 140, y: 150},  {x: 130, y: 150},  {x: 120, y: 150},  {x: 110, y: 150},],1);
+let test=new Draw();
+test.draw(snakeTest);
