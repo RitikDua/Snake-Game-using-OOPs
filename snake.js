f3583d5fc5cdcf6813e9dd770b007303b6e6eaf4
Ritik <ritik.cse18@chitkarauniversity.edu.in>
2020-04-27 15:03:20 +0530

    Basic Movement is completed
    Snake is moving and also eating food

--- snake.js
+++ snake.js
@@ -9,9 +9,48 @@ class Level{
 		
 	}
 }
+class Draw{
+
+	drawSnakePart(snakePart){
+		ctx.fillStyle="lightgreen";
+		ctx.strokeStyle="darkgreen";
+		ctx.fillRect(snakePart.x,snakePart.y,9,9);
+		ctx.strokeRect(snakePart.x,snakePart.y,9,9);
+			
+	}
+	drawSnake(snake){
+
+		snake.pos.forEach(this.drawSnakePart);
+	}
+	drawSomthing(thing){
+		console.log("drawSometghing"+thing.type);
+	}
+	drawApple(thing){
+		ctx.fillStyle="red";
+		ctx.strokeStyle="darkred";
+		ctx.fillRect(thing.x,thing.y,9,9);
+		ctx.strokeRect(thing.x,thing.y,9,9); 
+	}
+	draw(thing){
+		console.log((thing.type==="snake"));
+		if(thing.type==="snake")
+		{
+			this.drawSnake(thing);
+		}
+		else if(thing.type==="apple"){
+			this.drawApple(thing);
+		}
+		else{
+			this.drawSomthing(thing);
+		}
+	}
+}
+
 class Snake{
-	constructor(pos,speed){
+	constructor(x,y,pos,speed){
 		this.pos=pos;
+		this.x=y;
+		this.y=y;
 		this.speed=speed;
 	}
 	get type(){
@@ -19,98 +58,148 @@ class Snake{
 	}
 
 }
+let bug=0;
+class Apple{
+	constructor(x,y){
+		this.x=x;
+		this.y=y;
+	}
+	get type(){
+		return "apple";
+	}
 
-class Move{
+
+}
+class Move extends Draw{
 	constructor(runner,speed){
-		
+		super();
 		this.runner=runner;
 		this.dir="none";
 		this.interval="";
 		this.run=true;
+		this.dx=0;
+		this.dy=0;
 		this.speed=speed;
+		this.apple=new Apple(200,250);
 		// this.speed=Level.speed;
-		this.move();
+// 
+		// this.drawSnakeII=drawSnake;
+		console.log(this.runner.pos)
+		this.draw(this.apple);
 
-								console.log(this);
+		this.draw(this.runner);
+		this.move();						// console.log(this);
 
 
 	}
-	 move(){
-  ctx.clearRect(0, 0, canvas.width,canvas.height);// Clear the Canvas
-  	this.interval=setInterval(this.moveSnake(this),200);
-  // this.moveSnake();
-	}
-	moveSnake(){
-		document.addEventListener("keydown",(e)=>{
-			
-			if(this.run)
-			{switch(e.keyCode)
+
+
+	moveSnakeCanvas(){			
+		ctx.clearRect(0, 0, canvas.width,canvas.height);// Clear the Canvas
+		this.runner.x+=this.dx;
+
+		this.runner.y+=this.dy;
+		let snake=this.runner;
+
+		for(let i=0;i<this.runner.pos.length;i++)
 			{
-				case 37:console.log("left");
-						
-						if(this.dir!="right")
-						{	let here=this;
-							clearInterval(this.interval);
-
-							this.interval=setInterval(function(e){
-								
-								dx=-10;
-								dy=0;
-								 console.log(here);
-								 here.move();
-							},180);
-							this.dir="left";
-						}
-				break;
-				case 38:console.log("up");
-
-						if(this.dir!="down")
-						{	let here=this;
-							clearInterval(this.interval);
-							this.interval=setInterval(function(){
-								dx=-10;
-								dy=0;
-								console.log(here);
-								here.move();
-							},180);
-							this.dir="up";
-						}
-				break;
-				case 39:console.log("right");
-
-						if(this.dir!="left")
-						{let here=this;
-							clearInterval(this.interval);
-							this.interval=setInterval(function(){
-								dx=10;
-								dy=0;
-								console.log(here);
-								here.move();
-							},180);
-							this.dir="right";
-						}
-				break;
-				case 40:console.log("down");
-
-						if(this.dir!="up")
-						{let here=this;
-							clearInterval(this.interval);
-							this.interval=setInterval(function(){
-								dx=10;
-								dy=0;
-								console.log(here);
-								here.move();
-							},180);
-							this.dir="down";
-						}
-				break;
-				// case 32:this.			
 			}
+		  this.runner.pos.unshift({x: this.runner.x , y: this.runner.y}) ; // Insert at 0th position
+		  	console.log(this.apple.x);
+		  	console.log(this.apple.y);
+		  	console.log(this.runner);
+		  if(this.runner.x===this.apple.x && this.runner.y===this.apple.y){
+		  	this.apple.x=parseInt(Math.floor(Math.random()*24+1));
 
-		}
-		})
+			this.apple.y=parseInt(Math.floor(Math.random()*24+1));
+			
+			this.apple.x*=10;
+    		this.apple.y*=10;
+		  	// this.apple.x*=16;this.apple.*=16;
+		  }
+		  else this.runner.pos.pop();
+		this.draw(this.runner);
+		
 	}
+	move(){
+		let here=this;
 
+		this.draw(this.runner);
+		// this.moveSnakeCanvas();
+  		// setInterval((e)=>{
+  		// 	console.log(e);
+  		// here.dx=12;
+  		// here.moveSnakeCanvas();
+  		// 	},100)
+  		
+  		document.addEventListener('keydown',(e)=>{
+			if(this.run){
+				// let here=this;
+					switch(e.keyCode){
+
+						case 37: console.log("Left");
+								if(this.dir!="right")
+								{	if(this.interval!="")
+									clearInterval(this.interval);
+									let here=this;
+									console.log(here);
+									here.interval=setInterval((e)=>{
+										here.dx=-10;here.dy=0;
+										here.moveSnakeCanvas();
+										here.draw(here.apple);
+									},180);
+									this.dir='left';
+								}
+						break;
+						case 39: console.log("Right");
+								if(this.dir!="left")
+								{	if(this.interval!="")
+									clearInterval(this.interval);
+									let here=this;
+									console.log(here);
+									here.interval=setInterval((e)=>{
+										here.dx=10;here.dy=0;
+										here.moveSnakeCanvas();
+									
+										here.draw(here.apple);},180);
+									this.dir='right';
+								}
+						break;
+						case 38: console.log("Up");
+								if(this.dir!="down")
+								{	if(this.interval!="")
+									clearInterval(this.interval);
+									let here=this;
+									console.log(here);
+									here.interval=setInterval((e)=>{
+										here.dx=0;here.dy=-10;
+										here.moveSnakeCanvas();
+										here.draw(here.apple);
+									},180);
+									this.dir='up';
+								}
+						break;
+						case 40: console.log("Down");
+								if(this.dir!="up")
+								{	if(this.interval!="")
+									clearInterval(this.interval);
+									let here=this;
+									console.log(here);
+									here.interval=setInterval((e)=>{
+										here.dx=0;here.dy=10;
+										here.moveSnakeCanvas();
+										here.draw(here.apple);
+									},180);
+									this.dir='down';
+								}
+						break;
+						case 32: console.log("Paused");//forPause
+								clearInterval(this.interval);
+					}	
+			}
+		})
+
+	}
 
 }
 
@@ -124,33 +213,6 @@ function domElt(name,attrs,...children){
 		dom.appendChild(child);
 	}
 	return dom;
-}
-class Draw{
-
-	drawSnakePart(snakePart){
-		ctx.fillStyle="lightgreen";
-		ctx.strokeStyle="darkgreen";
-		ctx.fillRect(snakePart.x,snakePart.y,10,10);
-		ctx.strokeRect(snakePart.x,snakePart.y,10,10);
-			
-	}
-	drawSnake(snake){
-		snake.pos.forEach(this.drawSnakePart);
-	}
-	drawSomthing(thing){
-		console.log("drawSometghing"+thing.type);
-	}
-	
-	draw(thing){
-		console.log((thing.type==="snake"));
-		if(thing.type==="snake")
-		{
-			this.drawSnake(thing);
-		}
-		else{
-			this.drawSomthing(thing);
-		}
-	}
 }
 
 class State{
@@ -169,10 +231,11 @@ class State{
 	}
 	playing(){
 				
-		let snakeTest= new Snake([  {x: 150, y: 150},  {x: 140, y: 150},  {x: 130, y: 150},  {x: 120, y: 150},  {x: 110, y: 150},],1);
+		let snakeTest= new Snake(150,150,[  {x: 150, y: 150},  {x: 140, y: 150},  {x: 130, y: 150},  {x: 120, y: 150},  {x: 110, y: 150},],1);
 		let test=new Draw();
-		test.draw(snakeTest);
-		let move=new Move(snakeTest,this.level.speed);
+		// test.draw(snakeTest);
+		// console.log(test.draw);
+		let move=new Move(snakeTest,this.level.speed);//,new Draw());
 	}
 	static start(level){
 		return new State(level,level.startActors,"playing");
